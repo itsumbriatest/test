f2d5fef59fc7bdb4055573dd290821dfe5cdd486
DESKTOP-PPIKK34\ITS NOL39 <marcellobanano03@gmail.com>
2023-03-07 09:37:59 +0100

    Implementazione registro elettronico "Matteo Ceccarelli"

---
+++ digitale_b_2022_23/registro_elettronico/Matteo_Ceccarelli/Registro.py
@@ -0,0 +1,23 @@
+class Classe:
+    def __init__():
+        listaStudenti = ["Aldo", "Giovanni", "Giacomo"]
+        listaMaterie = ["Informatica", "Italiano", "Matematica"]
+        listaVoti = [range(0, 11)]
+
+class Studente:
+    def __init__():
+        pass
+
+class Materia:
+    def __init__():
+        pass
+
+class Voto:
+    def __init__():
+        pass
+
+# main
+m1 = Materia("Italiano")
+m2 = Materia("Matematica")
+s1 = Studente("Paolo", "Barnardi")
+s1 = 
