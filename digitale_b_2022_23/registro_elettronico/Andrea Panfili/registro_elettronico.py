c8da47e34eec80b23b5dd573a6c96f0c98e84428
Andrea Panfili <allievo_panfili.a@itsumbria.it>
2023-03-07 09:38:10 +0100

    Implementazione registro elettronico "Andrea Panfili"

---
+++ digitale_b_2022_23/registro_elettronico/Andrea Panfili/registro_elettronico.py
@@ -0,0 +1,76 @@
+class Classe:
+    def __init__(self):
+        andrea = Studente ("Andrea", "Panfili")
+        massi = Studente ("Massi", "Muzi")
+        self.lista_studenti_classe1 = [andrea, massi]
+
+        italiano = Materia ("Italiano")
+        matematica = Materia ("Matematica")
+        self.lista_materie_classe1 = [italiano, matematica]
+
+        v1 = Voto (7, andrea, italiano, "17/2/2023")
+        v2 = Voto (4, andrea, matematica, "11/2/2023")
+        v3 = Voto (9, massi, italiano, "17/2/2023")
+        v4 = Voto (6, massi, matematica, "11/2/2023")
+        self.lista_voti_classe1 = [v1, v2, v3, v4]
+        
+      
+        
+        #lorenzo = Studente ("Lorenzo", "Scianca")
+        #tommaso = Studente ("Tommaso", "Sabbi")
+        #self.lista_studenti_classe2 = [lorenzo, tommaso]
+        #
+        #storia = Materia ("Storia")
+        #scienze = Materia ("Scienze")
+        #self.lista_materie_classe2 = [storia, scienze]
+        #
+        #v5 = Voto (5, lorenzo, italiano, "17/2/2023")
+        #v6 = Voto (3, lorenzo, matematica, "11/2/2023")
+        #v7 = Voto (8, tommaso, italiano, "17/2/2023")
+        #v8 = Voto (10, tommaso, matematica, "11/2/2023")
+
+
+
+    def AggiungiVoto(self):
+        for i in range(len(self.lista_studenti_classe1)):
+            print(i, ")", self.lista_studenti_classe1[i])
+        nome_stud = int(input("Di quale studente vuoi inserire il nuovo voto?"))
+        stud = self.lista_materie_classe1[nome_stud]
+
+        for i in range(len(self.lista_materie_classe1)):
+            print(i, ")", self.lista_materie_classe1[i])
+        nome_mat = int(input("Di quale materia vuoi inserire il nuovo voto?"))
+        mat = self.lista_materie_classe1[nome_mat]
+
+        data = input("Inserisci la data del voto: ")
+        punteggio = float(input("Inserire il punteggio del voto: "))
+        voto = Voto(punteggio, stud, mat, data)
+
+        self.lista_voti_classe1.append(voto)
+
+
+    
+          
+
+
+
+class Studente:
+    #attributo nome e cognome
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
+        
+class Materia:
+    #attributo nome
+    def __init__(self, nome):
+        self.nome = nome
+
+
+class Voto:
+    def __init__(self, punteggio, studente, materia, data) -> None:
+        self.punteggio = punteggio
+        self.studente = studente
+        self.materia = materia
+        self.data = data
+        
+        
