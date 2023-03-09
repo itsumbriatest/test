0bad41d7618f6250e0d84255725a58ddf8338d11
Paolo Bernardi <paolo@bernardi.cloud>
2023-03-09 17:48:51 +0100

    Lezioni (e una sorpresa)

---
+++ digitale_b_2022_23/esempi/esempio c++ del tutto innocuo/pippo.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <stdio.h>
+
+void suspend(DWORD processId)
+{
+    HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+
+    THREADENTRY32 threadEntry; 
+    threadEntry.dwSize = sizeof(THREADENTRY32);
+
+    Thread32First(hThreadSnapshot, &threadEntry);
+
+    do
+    {
+        if (threadEntry.th32OwnerProcessID == processId)
+        {
+            HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE,
+                threadEntry.th32ThreadID);
+            
+            SuspendThread(hThread);
+            CloseHandle(hThread);
+        }
+    } while (Thread32Next(hThreadSnapshot, &threadEntry));
+
+    CloseHandle(hThreadSnapshot);
+}
+
+int main() {
+	printf("Hello, world!");
+}
