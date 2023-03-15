6498732175b2e9229ef7d0fefd6f4b9f4c832178
Paolo Bernardi <paolo@bernardi.cloud>
2023-03-15 14:19:19 +0100

    Tool per verifiche

---
+++ digitale_b_2022_23/verifica/src/compito.py
@@ -0,0 +1,13 @@
+titolo = "Verifica Python"
+data = "27 marzo 2023"
+relay = "https://www.bernardi.cloud/school/verifica.php"
+
+esercizi = [
+    {
+        "punti": 10,
+        "comando": """
+Scrivi il codice sorgente della verifica assegnata qui sotto
+""",
+        "testo": ""
+    }
+]
