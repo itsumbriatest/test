ec8aeeb20f142462c7c722121aeabb82d9332489
AzureAD\CrispoltiAlessandro <allievo_crispolti.a@itsumbria.it>
2023-04-13 17:32:03 +0200

    ok i pull up

---
+++ soa_2022_23/client_server/Alessandro Crispolti/esempio_server.py
@@ -0,0 +1,20 @@
+from paste import httpserver
+from bottle import default_app, route
+
+@route("/")
+def index():
+    return "Hello, world!"
+
+@route("/test2")
+def secondo_test():
+    return "Another test"
+
+@route("/test3")
+def secondo_test():
+    my_dizionario = {
+        "nome": "Alessandro", 
+        "cognome": "Del Piero"
+    }
+    return my_dizionario
+
+httpserver.serve(default_app(), host="127.0.0.1", port=8080)
