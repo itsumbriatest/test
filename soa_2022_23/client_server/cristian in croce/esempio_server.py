28ebe19a0e0d86363da02a875b25649676b24a5c
cristian rocco <christian.rocco1999@gmail.com>
2023-04-13 17:27:54 +0200

    no message

---
+++ soa_2022_23/client_server/cristian in croce/esempio_server.py
@@ -0,0 +1,20 @@
+from paste import httpserver
+from bottle import default_app, route
+
+@route("/")
+def index():
+    return "Hello, world!"
+
+@route("/test2")
+def secondo_test():
+    return "Another test"
+
+@route("/test3")
+def secondo_test():
+    my_dizionario = {
+        "nome": "cristian", 
+        "cognome": "in croce"
+    }
+    return my_dizionario
+
+httpserver.serve(default_app(), host="127.0.0.1", port=8080)
