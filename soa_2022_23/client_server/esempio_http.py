7f976f852454bda75b682355ecdc0c1397574691
Paolo Bernardi <paolo@bernardi.cloud>
2023-04-13 14:28:12 +0200

    Esempio di client REST

---
+++ soa_2022_23/client_server/esempio_http.py
@@ -0,0 +1,9 @@
+# pip install requests
+import requests
+url = "http://ipinfo.io"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    print(content)
+else:
+    print("Errore durante la richiesta HTTP")
