3bec034b8040c6d1daf1417a0dd02da915211eac
Paolo Bernardi <paolo@bernardi.cloud>
2023-04-13 14:57:01 +0200

    Further HTTP server example semplification

--- soa_2022_23/client_server/esempio_server.py
+++ soa_2022_23/client_server/esempio_server.py
@@ -1,12 +1,10 @@
 import os
 
-from paste import httpserver  # multithread HTTP server
-from bottle import default_app, post, request, route, run, template
-
+from paste import httpserver
+from bottle import default_app, route
 
 @route("/")
 def index():
     return "Hello, World!"
-
 
 httpserver.serve(default_app(), host="127.0.0.1", port=8080)
