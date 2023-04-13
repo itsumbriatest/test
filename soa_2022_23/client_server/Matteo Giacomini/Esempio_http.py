436cdce343e36c0c47fb86d96829504ea75e4f60
LAPTOP-0VM3GPC4\matte <matteogiacomini260203@gmail.com>
2023-04-13 16:35:40 +0200

    http

---
+++ soa_2022_23/client_server/Matteo Giacomini/Esempio_http.py
@@ -0,0 +1,8 @@
+import requests
+url = "http://ipinfo.io"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    print(content)
+else:
+    print("Errore durante la richiesta HTTP")
