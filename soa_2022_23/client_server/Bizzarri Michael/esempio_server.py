a3ab2e11c883da3893a05e887edf441bd41b2c65
Alsair03 <126595283+Alsair03@users.noreply.github.com>
2023-04-13 17:38:44 +0200

    client_server "Bizzarri Michael"

---
+++ soa_2022_23/client_server/Bizzarri Michael/esempio_server.py
@@ -0,0 +1,20 @@
+from paste import httpserver
+from bottle import default_app, route
+
+@route("/")
+def index():
+    return "Hello, world!"
+
+@route("/test2")
+def secondo_test():
+    return "Another test"
+
+@route("/test3")
+def secondo_test():
+    my_dizionario = {
+        "nome": "Michael", 
+        "cognome": "Bizzarri"
+    }
+    return my_dizionario
+
+httpserver.serve(default_app(), host="127.0.0.1", port=8080)
