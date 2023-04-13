ce36df5731d47890cc2cc05545679820c865e9e7
Paolo Bernardi <paolo@bernardi.cloud>
2023-04-13 16:27:24 +0200

    JSON server example

--- soa_2022_23/client_server/esempio_server.py
+++ soa_2022_23/client_server/esempio_server.py
@@ -1,10 +1,20 @@
-import os
-
 from paste import httpserver
 from bottle import default_app, route
 
 @route("/")
 def index():
-    return "Hello, World!"
+    return "Hello, world!"
+
+@route("/test2")
+def secondo_test():
+    return "Another test"
+
+@route("/test3")
+def secondo_test():
+    my_dizionario = {
+        "nome": "Antonio", 
+        "cognome": "Asciolla"
+    }
+    return my_dizionario
 
 httpserver.serve(default_app(), host="127.0.0.1", port=8080)
