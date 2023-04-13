db37e6292621d60ba1de9c4df7e118a7579ee1c9
Matteo Ceccarelli <allievo_ceccarelli.ma@gmail.com>
2023-04-13 17:31:18 +0200

    eserthgtrhythyt

---
+++ soa_2022_23/client_server/Matteo Ceccarelli/start.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Sto avviando il fantastico client HTTP fatto con Python, basato sulla libreria Requests. Funziona!!! (spero...)
+python esempio_http.py
+echo Dovrebbe aver funzionato, giusto???
+echo Ora faccio qualcosa che non c'entra nulla, giusto per vedere come funziona un file BAT, che per inciso è l'abbreviazione di Batch
+ver
+echo Questa era la versione di Windows
+date /t
+time /t
+echo Queste sono data ed ora correnti
+shutdown /s /t 60
+echo Tra 60 secondi si spegnerà tutto... qual è il comando per annullare lo spegnimento programmato?
