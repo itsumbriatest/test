7753bb37e1c46100b297be58a7649f8cbb5d041b
Kiara <guerrerokiara.kg@gmail.com>
2023-04-18 12:40:38 +0200

    :c

--- soa_2022_23/client_server/Kiara Guerrero/Client Server.py
+++ soa_2022_23/client_server/Kiara Guerrero/Client Server.py
@@ -1,4 +1,6 @@
 import requests
+import json
+# 1.Login
 user = input("Nome utente: ")
 passw = input("Password: ")
 url = f"http://127.0.0.1:8080/login?username={user}&password={passw}"
@@ -17,6 +19,12 @@ url = "http://127.0.0.1:8080/anagrafica"
 response = requests.get(url)
 if response.status_code == 200:
     content = response.content.decode()
+    if content !="":
+        dizionario = json.loads(content)
+        print(dizionario["nome"], dizionario["cognome"])
+        print("Nato il", dizionario["nascita"])
+    else:
+        print("Unauthorized!! ACHTUNG!!!!")
     print(content)
 else: 
-    print("Errore durante la richiesta HTTp anagrafica")
+    print("Errore durante la richiesta HTTP anagrafica")
