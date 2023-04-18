b8e432d140c1ef759745fef25f469d6ba2200c72
Paolo Bernardi <paolo@bernardi.cloud>
2023-04-18 12:38:02 +0200

    Esempio servizio con login

---
+++ soa_2022_23/esempi/esempio_01_login/myclient.py
@@ -0,0 +1,31 @@
+# pip install requests
+import requests
+import json
+
+# 1. Login
+user = input("Nome utente: ")
+passw = input("Password: ")
+url = f"http://127.0.0.1:8080/login?username={user}&password={passw}"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    if content == "OK":
+        print("Login successful")
+    else:
+        print("Wrong user name or password")
+else:
+    print("Errore durante la richiesta HTTP")
+
+# 2. Anagrafica
+url = "http://127.0.0.1:8080/anagrafica"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    if content != "":
+        dizionario = json.loads(content)
+        print(dizionario["nome"], dizionario["cognome"])
+        print("Nato il", dizionario["nascita"])
+    else:
+        print("Unauthorized!!! ACHTUNG!!!")
+else:
+    print("Errore durante la richiesta HTTP anagrafica")
---
+++ soa_2022_23/esempi/esempio_01_login/myserver.py
@@ -0,0 +1,30 @@
+from paste import httpserver
+from bottle import default_app, request, route
+
+login_ok = False
+
+@route("/login")
+def login():
+    global login_ok
+    params = request.query.decode()
+    if params["username"] == "pippo" and params["password"] == "topolino":
+        login_ok = True
+        return "OK"
+    else:
+        login_ok = False
+        return "KO"
+
+@route("/anagrafica")
+def anagrafica():
+    global login_ok
+    anag = {
+        "nome": "Piero",
+        "cognome": "Brandola",
+        "nascita": "1982-01-16"
+    }
+    if login_ok:
+        return anag
+    else:
+        return False
+
+httpserver.serve(default_app(), host="127.0.0.1", port=8080)
