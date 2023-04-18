0952c5006a64297f53a27661dc39bd8fe1e60f5d
AzureAD\PagliaruloManuel <allievo_pagliarulo.m@itsumbria.it>
2023-04-18 12:40:48 +0200

    manuel

--- soa_2022_23/client_server/Manuel Pagliarulo/client.py
+++ soa_2022_23/client_server/Manuel Pagliarulo/client.py
@@ -1,5 +1,6 @@
 # pip install request
 import requests
+import json
 user = input("nome utente: ")
 passw = input("Password: ")
 url = f"http://127.0.0.1:8080/login?username={user}&password={passw}"
@@ -19,5 +20,19 @@ if response.status_code ==200:
     if content == "OK":
         content = response.content.decode()
         print(content)
+else:
+    print("errore durante la richiesta HTTP anagrafica")
+
+#2  anagrafica
+url = "http://127.0.0.1:8080/anagrafica"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    if content !="":
+        dizionario = json.loads(content)
+        print(dizionario["nome"],dizionario["cognome"])
+        print("nato il ",dizionario["nascita"])
+    else:
+        print("unathorized!!! ACHTUNG!!")
 else:
     print("errore durante la richiesta HTTP anagrafica")
