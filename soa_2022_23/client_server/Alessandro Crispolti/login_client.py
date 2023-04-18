b8f6e43fc617d88868db612ab155f988702f506c
AzureAD\CrispoltiAlessandro <allievo_crispolti.a@itsumbria.it>
2023-04-18 12:17:10 +0200

    modifica

---
+++ soa_2022_23/client_server/Alessandro Crispolti/login_client.py
@@ -0,0 +1,24 @@
+import requests
+
+# 1. Login
+user = input("Nome utente: ")
+passw = input("Password: ")
+url = f"http://127.0.0.1:8080/login?username={user}&password={passw}"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    if content == "OK":
+        print("Login effettuato")
+    else:
+        print("Username o password errati")
+else:
+    print("Errore durante la richiesta HTTP")
+
+# 2. Anagrafica
+url = "http://127.0.0.1:8080/anagrafica"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    print(content)
+else:
+    print("Errore durante la richiesta HTTP anagrafica")
