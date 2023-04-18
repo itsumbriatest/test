773ca414e1ab721f6791a0191e86ddc5dabab213
cristian rocco <christian.rocco1999@gmail.com>
2023-04-18 12:02:09 +0200

    no message

--- soa_2022_23/client_server/cristian in croce/esercizio server /esempio_http.py
+++ soa_2022_23/client_server/cristian in croce/esercizio server /esempio_http.py
@@ -2,6 +2,8 @@
 import requests
 nome = input("nome:")
 psw = input("password:")
+
+# 1) Login
 url = f"http://127.0.0.1:8080/login?username={nome}&password={psw}"
 response = requests.get(url)
 if response.status_code == 200:
@@ -10,6 +12,15 @@ if response.status_code == 200:
         print ("login effetuato")
     else :
         print ("user o password errate")
-    print(content)
 else:
     print("Errore durante la richiesta HTTP")
+
+# 2) Anag
+url = f"http://127.0.0.1:8080/anagrafica"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.content.decode()
+    print (content)
+else:
+    print("errore durante la richiesta http angarafica")
+
--- soa_2022_23/client_server/cristian in croce/esercizio server /server.py
+++ soa_2022_23/client_server/cristian in croce/esercizio server /server.py
@@ -1,17 +1,32 @@
 from paste import httpserver
 from bottle import default_app, request, route
 
+login_ok = False
+
 @route("/login")
 def login():
+    global login_ok 
     params = request.query.decode()
     u = params ["username"]
     p = params ["password"]
     print(f"Nuova login: '{u}' '{p}'")
     if  u == "cristian" and p == "123":
+        login_ok = True
         return "ok"
     else:
         return "ko"
 
-
-
+@route ("/anagrafica")
+def anagrafica ():
+    global login_ok
+    anag = {
+        "nome": "cristian", 
+        "cognome": "in croce",
+        "nascita": "1987-04-18"
+    }
+    if login_ok:
+        return anag
+    else:
+        return False
+    
 httpserver.serve(default_app(), host="127.0.0.1", port=8080)
