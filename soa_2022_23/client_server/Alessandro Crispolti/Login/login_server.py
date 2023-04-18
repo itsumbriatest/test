e4a564dbac16176fb480028164bf0f53bfe12e7b
AzureAD\CrispoltiAlessandro <allievo_crispolti.a@itsumbria.it>
2023-04-18 12:10:55 +0200

    Client Server Login

---
+++ soa_2022_23/client_server/Alessandro Crispolti/Login/login_server.py
@@ -0,0 +1,28 @@
+from paste import httpserver
+from bottle import default_app, request, route
+
+login_ok = False
+@route("/login")
+def login():
+    global login_ok
+    params = request.query.decode()
+    if params["username"] == "Alex10543!?" and params["password"] == "123456789":
+        login_ok = True
+        return "OK"
+    else:
+        return "KO"
+    
+@route("/anagrafica")
+def anagrafica():
+    global login_ok
+    anag = {
+        "nome":"Alessandro",
+        "cognome":"Crispolti",
+        "nascita":"2002-07-29"
+    }
+    if login_ok == True:
+        return anag
+    else:
+        return False
+
+httpserver.serve(default_app(), host="127.0.0.1", port=8080)
