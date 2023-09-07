0a8f2e40c4104b10bb70a7f330a6302b39afe110
Paolo Bernardi <paolo@bernardi.cloud>
2023-09-07 15:45:14 +0200

    Stub per decifrare Cesare

---
+++ cysec_2022_23/decifra_cesare.py
@@ -0,0 +1,8 @@
+# IMPLEMENTA UN ATTACCO BRUTE-FORCE
+# CONTRO IL CIFRARIO DI CESARE
+
+import cesare
+
+if __name__ == "__main__":
+    cipher_text = "CRUUR"
+    print("Proviamo tutte le chiavi con", cipher_text)
